076a279f9d46dc163a4ca4bb1bbab189a80f54ae
Rahul Soshte <rahul.soshte47@gmail.com>
2016-12-23 21:47:35 +0530

    Showed the Ebook.txt file in the console

---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using System.IO;
+
+
+public class TextController : MonoBehaviour {
+
+ void Start(){	
+
+		//FileInfo theSourceFile = null;
+		StringReader reader = null;
+	
+
+		TextAsset Ebook = (TextAsset)Resources.Load ("Ebook", typeof(TextAsset));
+		//Ebook.txt is a string containing  the whole file.TO read it line by line
+		reader = new StringReader(Ebook.text);
+		if (reader == null) {
+			Debug.Log ("Ebook.txt not found or readable");
+
+		} else {
+			//Read Each line from file
+			string txt;
+
+			while((txt=reader.ReadLine())!=null)
+				{
+					Debug.Log("-->"+txt);
+		
+				}
+		}
+	}
+
+
+}
+
+
