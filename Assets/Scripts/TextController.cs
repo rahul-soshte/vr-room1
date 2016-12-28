07aa0f6b13d14319ad99028f8c6fc65aed4f51ee
Rahul Soshte <rahul.soshte47@gmail.com>
2016-12-29 03:28:23 +0530

    Showed the whole ebook in the GUI text

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -6,12 +6,26 @@ using System.IO;
 
 
 public class TextController : MonoBehaviour {
+	private Text txt;//the string to be displayed in GUI Text element attached
+	protected int lowBound=0;
+	protected int upBound=0;
+
+	 protected string completeText="";
 
  void Start(){	
 
-		//FileInfo theSourceFile = null;
+		showInConsole ();
+
+
+	}
+
+	void Update(){
+		showInGuiText ();
+
+	}
+	void showInConsole(){
+
 		StringReader reader = null;
-	
 
 		TextAsset Ebook = (TextAsset)Resources.Load ("Ebook", typeof(TextAsset));
 		//Ebook.txt is a string containing  the whole file.TO read it line by line
@@ -24,14 +38,36 @@ public class TextController : MonoBehaviour {
 			string txt;
 
 			while((txt=reader.ReadLine())!=null)
-				{
-					Debug.Log("-->"+txt);
-		
-				}
+			{
+				completeText = completeText + txt + "\n";
+
+
+			}
 		}
+		}
+
+	void showInGuiText(){
+		txt = GetComponent<Text> ();
+	int length = completeText.Length;
+
+			if (Input.GetKey (KeyCode.RightArrow)) {
+			lowBound = upBound;
+			upBound = lowBound + 150;
+			txt.text = completeText.Substring (lowBound, upBound);
+			}
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+			upBound = lowBound;
+			lowBound = upBound - 150;
+		
+			txt.text = completeText.Substring (lowBound, upBound);
+
+			}
+
+	
+	}
 	}
 
 
-}
+
 
 
